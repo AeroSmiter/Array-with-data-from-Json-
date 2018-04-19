664550e6b7ff200ff0bd613fdf92eb89053f6625
Daniel Przerwa <zaczynaczek@gmail.com>
2018-04-19 21:58:29 +0200

    0.02
    Poprawiono wyświetlanie table.
    Prostsze dodawanie danych.

--- script/script.js
+++ script/script.js
@@ -1,20 +1,15 @@
-$.ajax({ 
-    type: 'GET', 
-    url: 'https://jsonplaceholder.typicode.com/users', 
-    data: { get_param: 'value' }, 
-    dataType: 'json',
-    success: function (data) { 
-        $.each(data, function(index, element) {
-            $('tbody').append($('<tr>', {
-                text: element.id + "." 
-            }));
-			
-			$('tbody').append($('<td>', {
-                text: element.name
-            }));
-			
+$.ajax({
+    url: 'https://jsonplaceholder.typicode.com/users',
+    data: {get_param: 'value'},
+    dataType: "json",
+        cache: false,
+        success: function (data) {
+        $.each(data, function (i, val) {
+            var tr = "<tr>" +
+                "<td>"+ (i + 1) + "</td>" +
+                "<td>"+ val.name + "</td>" +
+                "</tr>";
+            $(tr).appendTo("tbody");
         });
     }
-	
 });
-
