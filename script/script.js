f2e2fc21924d9fb9959aaf45868b3a32a2a69db0
Daniel Przerwa <zaczynaczek@gmail.com>
2018-04-20 21:39:44 +0200

    0.03
    Milena usprawniła kod. Wyciągając generator TABLE na zewnątrz.
    Został dodany klawisz buttonShow i animacja.
    Usprawniono działanie klawisza buttonShow

--- script/script.js
+++ script/script.js
@@ -1,15 +1,28 @@
-$.ajax({
-    url: 'https://jsonplaceholder.typicode.com/users',
-    data: {get_param: 'value'},
-    dataType: "json",
-        cache: false,
-        success: function (data) {
-        $.each(data, function (i, val) {
-            var tr = "<tr>" +
-                "<td>"+ (i + 1) + "</td>" +
-                "<td>"+ val.name + "</td>" +
-                "</tr>";
-            $(tr).appendTo("tbody");
-        });
-    }
+
+
+function load()
+{
+	$.ajax({
+		url: 'https://jsonplaceholder.typicode.com/users',
+		data: {get_param: 'value'},
+		dataType: "json",
+			cache: false,
+			success: succesResult
+		}
+	);	
+};
+
+$("#buttonShow").click(function(drawTable){
+	load();
 });
+
+function succesResult(data) {
+	$.each(data, function (i,val) {
+		var tr = "<tr>"+
+			"<td>" + val.id + "</td>" +
+			"<td>" + val.name + "</td>" +
+			"<td>" + val.username + "</td>" +
+			"</tr>";
+		$(tr).appendTo("tbody");
+	});
+}
